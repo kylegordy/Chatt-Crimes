089215c3eec2cefed1a28d1ecba50c7e6c0bddfa
Kyle Gordy <kyle@kylegordydesign.com>
2014-03-03 21:54:32 -0500

    rewrote menu.js in plain js and removed jQuery

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,13 +1,22 @@
-$('body').addClass("active-sidebar");
+// plain js functions are from this awesome site
+// http://toddmotto.com/creating-jquery-style-functions-in-javascript-hasclass-addclass-removeclass-toggleclass/
 
-var showMenu = function() {
-  $('body').toggleClass("active-sidebar"); 
+function hasClass(elem, className) {
+    return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
 }
 
-jQuery(document).ready(function($) {
-  // Toggle for nav menu
-  $('.menu').click(function(e) {
-    e.preventDefault();
-    showMenu();             
-  });            
-});
+function toggleClass(elem, className) {
+    var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ' ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace( ' ' + className + ' ' , ' ' );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+document.getElementById('menu').onclick = function() {
+    toggleClass(document.documentElement, 'active-sidebar');
+}
