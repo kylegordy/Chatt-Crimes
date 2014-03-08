abbae1c7e3bc680064089975ff0423ec7275bf59
Kyle Gordy <kyle@kylegordydesign.com>
2014-03-08 02:34:18 -0500

    removed gradient and clear function

--- assets/js/map.js
+++ assets/js/map.js
@@ -91,16 +91,8 @@ function selectedData(crimeType){
   });
 
   heatmap.setMap(map);
- 
 
 }
-
-// Clear Heatmap of any data
-function clearHeatmap() {
-  heatmap.setMap();
-}
-
-
 
 
 // Geolocation Error Notice
@@ -120,29 +112,6 @@ function handleNoGeolocation(errorFlag) {
 
   var infowindow = new google.maps.InfoWindow(options);
   map.setCenter(options.position);
-}
-
-// Change Gradient Button
-function changeGradient() {
-  var gradient = [
-    'rgba(0, 255, 255, 0)',
-    'rgba(0, 255, 255, 1)',
-    'rgba(0, 191, 255, 1)',
-    'rgba(0, 127, 255, 1)',
-    'rgba(0, 63, 255, 1)',
-    'rgba(0, 0, 255, 1)',
-    'rgba(0, 0, 223, 1)',
-    'rgba(0, 0, 191, 1)',
-    'rgba(0, 0, 159, 1)',
-    'rgba(0, 0, 127, 1)',
-    'rgba(63, 0, 91, 1)',
-    'rgba(127, 0, 63, 1)',
-    'rgba(191, 0, 31, 1)',
-    'rgba(255, 0, 0, 1)'
-  ]
-  heatmap.setOptions({
-    gradient: heatmap.get('gradient') ? null : gradient
-  });
 }
 
 // Initialize Map
