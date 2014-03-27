33f7a0f6f556a93242a9b1ca4fe80b9d34a54077
juzten <juzten@gmail.com>
2014-03-26 23:03:47 -0400

    fixed background state of current menu item clicked, now when you click a type of data in the menu bar the seleected menu option stays highlighted and all others...unhightlight.

--- assets/js/map-v2.js
+++ assets/js/map-v2.js
@@ -8,18 +8,18 @@ function showlocation() {
 
 // Locate the position
 function callback(position) {
-   
+
   var lat = position.coords.latitude;
   var lon = position.coords.longitude;
-       
+
   var latLong = new google.maps.LatLng(lat, lon);
-   
+
   var marker = new google.maps.Marker({
       position: latLong,
       icon: 'assets/images/icons/map-icon.png',
       animation: google.maps.Animation.DROP
   });
-           
+
   marker.setMap(map);
   map.setZoom(17);
   map.setCenter(marker.getPosition());
@@ -43,36 +43,54 @@ function errorHandler(error) {
     }
   }
 
+
+// function to reset background colors of menu items
+function resetMenuBackground() {
+  document.getElementById('myLocation').style.background ='';
+  document.getElementById('aggravatedAssault').style.background ='';
+  document.getElementById('autoTheft').style.background ='';
+  document.getElementById('burglary').style.background ='';
+  document.getElementById('drugs').style.background ='';
+  document.getElementById('homicide').style.background ='';
+  document.getElementById('theft').style.background ='';
+}
+
 // Select Crime Function
 function selectedData(crimeType){
-  heatmap.setMap();
+  resetMenuBackground();
 
   if (crimeType === 'autoTheft')
     {
       pointArray = autoTheft;
+      document.getElementById('autoTheft').style.background ='#cbcbcb';
     }
   else if (crimeType === 'aggravatedAssault')
     {
       pointArray = aggravatedAssault;
+      document.getElementById('aggravatedAssault').style.background ='#cbcbcb';
     }
   else if (crimeType === 'burglary')
     {
       pointArray = burglary;
+      document.getElementById('burglary').style.background ='#cbcbcb';
     }
   else if (crimeType === 'homicide')
     {
       pointArray = homicide;
+      document.getElementById('homicide').style.background ='#cbcbcb';
     }
   else if (crimeType === 'theft')
     {
       pointArray = theft;
+      document.getElementById('theft').style.background ='#cbcbcb';
     }
   else if (crimeType === 'drugs')
     {
       pointArray = drugs;
+      document.getElementById('drugs').style.background ='#cbcbcb';
     }
 
-  // pointArray = autoTheft;
+  heatmap.setMap();
   heatmap = new google.maps.visualization.HeatmapLayer({
     data: pointArray,
     radius: 25,
@@ -109,5 +127,5 @@ function initMap() {
   });
 
   heatmap.setMap(map);
- 
+
 }
