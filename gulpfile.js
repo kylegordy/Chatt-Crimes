25633799310cd3f176fa79bbda267118917bc2a5
kylegordy <kyle@kylegordydesign.com>
2014-08-06 22:08:57 -0400

    added gulp task to lint, concatenate and minify js, moved lodash.js and spin.js into lib folder to be concatenated

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var browserSync = require('browser-sync');
 var notify = require('gulp-notify');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 // compile sass and check for errors
 gulp.task('compile-sass', function () {
@@ -14,9 +17,24 @@ gulp.task('compile-sass', function () {
         .pipe(gulp.dest('assets/css'));
 });
 
+// lint js
+gulp.task('lint-js', function() {
+    return gulp.src('./assets/dev/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// concatenate & minify js
+gulp.task('concat-js', function() {
+    gulp.src(['./assets/js/lib/lodash.compat.min.js', './assets/js/lib/spin.min.js', './assets/js/dev/data.js', './assets/js/dev/map.js', './assets/js/dev/menu.js'])
+        .pipe(concat('global.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./assets/js/dist/'))
+});
+
 // start browser-sync and watch for html, css and js changes
 gulp.task('browser-sync', function() {  
-    browserSync.init(["assets/css/*.css", "*.html", "assets/js/*.js"], {
+    browserSync.init(["assets/css/*.css", "*.html", "assets/js/dev/*.js"], {
         server: {
             baseDir: "./"
         }
@@ -24,6 +42,7 @@ gulp.task('browser-sync', function() {
 });
 
 // watch for changes
-gulp.task('default', ['compile-sass', 'browser-sync'], function () {  
+gulp.task('default', ['compile-sass', 'lint-js', 'concat-js', 'browser-sync'], function () {  
     gulp.watch("assets/sass/**/*.scss", ['compile-sass']);
+    gulp.watch("assets/js/dev/*.js", ['lint-js', 'concat-js']);
 });
